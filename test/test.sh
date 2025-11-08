fe26e592c70617518959419c0a12796005aad1b9
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2025-11-08 21:13:15 +0000

    Updated tests

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Test binary by running various commands
 #
@@ -10,18 +10,29 @@ abs_path_to_enclosing_dir () {
 TEST_ROOT=$(abs_path_to_enclosing_dir $0)
 BIN_PATH="products/hear"
 
-EXPECTED_OUTPUT="The rain in Spain stays mainly in the plain"
                  
 test_transcribe_file() {
-    OUTPUT=$("$BIN_PATH" -d -i "$1")
-    if [ "$OUTPUT" != "$EXPECTED_OUTPUT" ]; then
+    OUTPUT=$("$BIN_PATH" -d -i "$1" -l "$2")
+    if [ "$OUTPUT" != "$3" ]; then
         echo "Unexpected output: ${OUTPUT}"
         exit 1
     fi
 }
 
-WAV_PATH="$TEST_ROOT/test.wav"
-test_transcribe_file $WAV_PATH
+# en-US
+EXPECTED_OUTPUT="The rain in Spain stays mainly in the plain"
+LOCALE="en-US"
 
-MP3_PATH="$TEST_ROOT/test.mp3"
-test_transcribe_file $MP3_PATH
+WAV_PATH="$TEST_ROOT/en-US.wav"
+test_transcribe_file $WAV_PATH $LOCALE "$EXPECTED_OUTPUT"
+
+MP3_PATH="$TEST_ROOT/en-US.mp3"
+test_transcribe_file $MP3_PATH $LOCALE "$EXPECTED_OUTPUT"
+
+# fr-FR
+EXPECTED_OUTPUT="Ça soir je vais à la maison"
+LOCALE="fr-FR"
+
+WAV_PATH="$TEST_ROOT/fr-FR.wav"
+test_transcribe_file $WAV_PATH $LOCALE "$EXPECTED_OUTPUT"
+
